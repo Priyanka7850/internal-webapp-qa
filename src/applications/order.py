7ee375b28f3da51626f609b584edc03e4b90882d
Priyanka7850 <priyankagodase785@gmail.com>
2025-03-25 02:55:01 +0900

    Create order.py

---
+++ src/applications/order.py
@@ -0,0 +1,11 @@
+
+
+class Order:
+    def __init__(self, user, items, total):
+        self.user = user
+        self.items = items
+        self.total = total
+        self.status = 'Pending'
+
+    def complete_order(self):
+        self.status = 'Completed'
