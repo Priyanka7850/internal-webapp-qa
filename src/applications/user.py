e049c9d0f971fbeccee1539f2b41efe5172bb7b2
Priyanka7850 <priyankagodase785@gmail.com>
2025-03-25 02:52:21 +0900

    Create user.py

---
+++ src/applications/user.py
@@ -0,0 +1,12 @@
+
+
+class User:
+    def __init__(self, id, username, email, password_hash):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+
+    def check_password(self, password):
+        # Implement password verification logic
+        pass
